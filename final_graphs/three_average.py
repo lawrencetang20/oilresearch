eb2bd6c1f3200a22b87ba03913390ba5a6ec3a93
lawrence tang <lawrence.j.tang@gmail.com>
2024-01-09 20:07:12 -0500

    finished average graph

--- final_graphs/three_average.py
+++ final_graphs/three_average.py
@@ -1,8 +1,18 @@
+import matplotlib.pyplot as plt
 from three_series import matching, statistica, investing_com, bloomberg
 
 def getAverage(matching, statistica, investing_com, bloomberg):
   average = [round(sum((statistica[i], investing_com[i], bloomberg[i]))/3,2) for i in range(len(matching))]
   return average
 
-def
+def plotAverage(matching, average):
+  plt.plot(matching, average)
+  plt.title('Urals Discounts')
+  plt.xlabel('Date')
+  plt.ylabel('Price Difference')
+  plt.xticks(matching[::30], rotation=45)
+  plt.tight_layout()
+  plt.show()
 
+average = getAverage(matching, statistica, investing_com, bloomberg)
+plotAverage(matching, average)
