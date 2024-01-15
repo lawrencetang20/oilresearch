972bc96029e25be8864c57f438f6da38492e3ea7
lawrence tang <lawrence.j.tang@gmail.com>
2024-01-15 14:21:56 -0500

    added relevant news articles to find out what happened in august

---
+++ test_graphs/urals_only.py
@@ -0,0 +1,7 @@
+import sys
+sys.path.append('/Users/lawrencetang/Desktop/Internships:Research/UROP/oilresearch')
+
+from final_products import data_functions as data
+import matplotlib.pyplot as plt
+
+bloomberg_urals = '../excel_csv/'
