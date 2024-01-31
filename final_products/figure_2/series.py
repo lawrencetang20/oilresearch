25dc98c557e6b311c2fbf0aaea4eb03a879f3ef3
lawrence tang <lawrence.j.tang@gmail.com>
2024-01-31 14:46:37 -0500

    added figure 2

---
+++ final_products/figure_2/series.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+from setup import EU_volume, india_volume, others_volume, turkey_volume, china_volume, US_UK_volume, dates
+
+def stackedPlot(EU, india, others, turkey, china, US_UK, dates):
+  categories = ['EU', 'US/UK', 'China', 'India', 'Turkey', 'Others']
+  plt.figure(figsize=(12, 6))
+  plt.stackplot(dates, EU, US_UK, china, india, turkey, others, labels=categories, baseline='zero', alpha=0.5)
+  plt.ylabel('Volume (million barrels per day)')
+  plt.xticks(range(0, len(dates), 30), [dates[i] for i in range(0, len(dates), 30)], rotation=45)
+  plt.legend(loc='upper left', bbox_to_anchor=(1.02, 1), borderaxespad=0)
+  plt.tight_layout()
+  plt.savefig('../../saved_pngs/figure_2/figure_2.png')
+  print('png saved')
+  plt.show()
+
+if __name__ == "__main__":
+  stackedPlot(EU_volume, india_volume, others_volume, turkey_volume, china_volume, US_UK_volume, dates)
---
+++ final_products/figure_2/setup.py
@@ -0,0 +1,5 @@
+import data_functions as data
+
+figure_2 = '../../excel_csv/figure_2/figure_2.csv'
+
+EU_volume, india_volume, others_volume, turkey_volume, china_volume, US_UK_volume, dates = data.extractData(figure_2)
