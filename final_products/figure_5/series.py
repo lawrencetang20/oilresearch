4614213842e75b72a89d74ec3ebf4ec7d368146b
lawrence tang <lawrence.j.tang@gmail.com>
2024-02-05 15:26:44 -0500

    changed colormap

--- final_products/figure_5/series.py
+++ final_products/figure_5/series.py
@@ -1,6 +1,6 @@
 from setup import points
 import matplotlib.pyplot as plt
-from matplotlib.colors import Normalize
+from matplotlib.colors import Normalize, LinearSegmentedColormap
 from matplotlib.cm import ScalarMappable
 import numpy as np
 
@@ -13,17 +13,34 @@ def regressionPlot(data):
   date_values = [int(date.split('-')[1]) for date in dates]
 
   fig, ax = plt.subplots(figsize=(12, 6))
+
+  pre_colors = plt.cm.Reds(np.linspace(0.8, 0.2, 256))  
+  post_colors = plt.cm.Blues(np.linspace(0.2, 0.8, 256))  
+
   norm = Normalize(vmin=min(date_values), vmax=max(date_values))
-  cmap = plt.get_cmap('coolwarm')
-  cmap = cmap.reversed()
-  scalar_map = ScalarMappable(norm=norm, cmap=cmap)
+  scalar_map = ScalarMappable(norm=norm, cmap=None)
 
   for i, label in enumerate(labels):
-    color = scalar_map.to_rgba(date_values[i])
-    if label == 'Post':
-      ax.scatter(x_values[i], y_values[i], color=color, label=label)
-    elif label == 'Pre':
-      ax.scatter(x_values[i], y_values[i], color=color, label=label)
+    if label == 'Pre':
+      color = pre_colors[int((date_values[i] - min(date_values)) / (max(date_values) - min(date_values)) * 255)]
+    elif label == 'Post':
+      color = post_colors[int((date_values[i] - min(date_values)) / (max(date_values) - min(date_values)) * 255)]
+    ax.scatter(x_values[i], y_values[i], color=color)
+
+  ax.set_xlabel('Extraction (million barrels per day)')
+  ax.set_ylabel('Price ($ per barrel)')
+  ax.set_title('Scatter Plot of Points')
+
+  # Create colorbars for 'Pre' and 'Post' gradients
+  pre_cbar = plt.colorbar(ScalarMappable(norm=norm, cmap=plt.cm.Reds), pad=0.02, ax=ax, orientation='vertical')
+  post_cbar = plt.colorbar(ScalarMappable(norm=norm, cmap=plt.cm.Blues), pad=0.02, ax=ax, orientation='vertical')
+  pre_tick_labels = ['', '2016', '', '', '', '', '', '', '2007']
+  post_tick_labels = ['', '2016', '', '', '', '', '', '', '2022']
+  pre_cbar.set_ticklabels(pre_tick_labels)
+  post_cbar.set_ticklabels(post_tick_labels)
+
+  pre_cbar.set_label('Pre Opec')
+  post_cbar.set_label('Post Opec')
 
   coeffs = np.polyfit(x_values, y_values, 1)
   slope = coeffs[0]
@@ -31,18 +48,10 @@ def regressionPlot(data):
   regression_line = [slope * x + intercept for x in x_values]
   ax.plot(x_values, regression_line, color='black', label='Regression Line')
 
-  ax.set_xlabel('Extraction (million barrels per day)')
-  ax.set_ylabel('Price ($ per barrel)')
-  ax.set_title('Scatter Plot of Points')
-
-  cbar = plt.colorbar(ScalarMappable(norm=norm, cmap=cmap), pad=0.02)
-  cbar.set_ticks([])
-  
   plt.tight_layout()
   plt.savefig('../../saved_pngs/figure_5/regression.png')
   print('png saved')
   plt.show()
-
 
 if __name__ == "__main__":
   regressionPlot(points)
